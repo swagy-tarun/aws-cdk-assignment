f4309ba87cb5950b9598b2fefc63083ee4a82eb0
Tarun <tarun@Taruns-iMac.local>
2024-12-13 10:26:38 +0530

    Initial commit

---
+++ test/aws-cdk-assignment.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const AwsCdkAssignment = require('../lib/aws-cdk-assignment-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-assignment-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AwsCdkAssignment.AwsCdkAssignmentStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
